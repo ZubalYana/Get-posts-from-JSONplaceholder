5beeb650ad023ab85a6aade999dfc3357c177d16
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-01-06 06:28:53 +0200

    add pagination, update styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,20 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const postsPerPage = 20;
+
   function getPosts() {
     axios
       .get('http://localhost:3000/posts') 
       .then((response) => {
-        const postsContainer = document.querySelector('.postsContainer');
-        postsContainer.innerHTML = '';
-        console.log(response.data)
-        response.data.forEach((post) => {
-          const postElement = document.createElement('div');
-          postElement.className = 'post';
-          postElement.innerHTML = `<h4 class="postTitle">${post.title}</h4><p class="postBody">${post.body}</p>`;
-          postsContainer.appendChild(postElement);
-        });
+        setPosts(response.data);
       })
       .catch((error) => {
         console.error('Error fetching posts:', error);
-        const postsContainer = document.querySelector('.postsContainer');
-        postsContainer.innerHTML = '<p>Error fetching posts. Please try again later.</p>';
       });
   }
 
@@ -28,10 +22,36 @@ function App() {
     getPosts();
   }, []);
 
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <div className="wrap">
       <h3>JSONplaceholder posts:</h3>
-      <div className="postsContainer"></div>
+      <div className="postsContainer">
+        {currentPosts.map((post) => (
+          <div className="post" key={post.id}>
+            <h4 className="postTitle">{post.title}</h4>
+            <p className="postBody">{post.body}</p>
+          </div>
+        ))}
+      </div>
+      <div className="pagination">
+        {Array.from({ length: Math.ceil(posts.length / postsPerPage) }, (_, index) => (
+          <button
+            key={index}
+            onClick={() => paginate(index + 1)}
+            className={currentPage === index + 1 ? 'active' : ''}
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
